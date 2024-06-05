6d40e8c7ef953a74397629b01b6f780efbe5e061
aabhishek777 <aabhishekksinghh@gmail.com>
2024-06-05 23:57:57 +0530

    updation in loop file

--- 05_loop.sh
+++ 05_loop.sh
@@ -19,3 +19,38 @@ for i in $(cat a.txt)
   echo "------------------${i}"
 done
 
+array=( 1 2 3 4 5 )
+# Running for loop in bash
+for (( i=0 ; i<${#array[*]} ; i++))
+ do 
+  echo "value-- ${array[i]}"
+done
+
+
+
+
+
+# reading the csv
+
+cat loop.csv | while IFS="," read id name add #IFS internal field separetor
+do 
+    echo "id is : $id"
+done
+
+
+cat loop.csv | awk 'NR!=1 {print}' | while IFS="," read id name add # awk use to remove first line //NR!=1' means first line will not include
+do 
+    echo "id is : $id"
+done
+
+
+
+
+# runnnig infinite
+for (( ; ; ))
+ do 
+ echo $(pwd)
+ sleep 2s
+done
+
+
